79dd536f7e0c4e64ecf1374c672b477241ab0451
khai93 <33293519+khai93@users.noreply.github.com>
2020-02-16 21:46:30 -0600

    Added player move with jump
    came up with the game final idea to be about fighting endless amount of bosses/monsters

---
+++ ROOOOAAGAAA/Assets/Code/PlayerJump.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    [SerializeField]
+    private Transform GroundCheck;
+
+    [SerializeField]
+    private LayerMask groundLayer;
+
+    [SerializeField]
+    private KeyCode jumpKey;
+
+    [SerializeField]
+    private float jumpForce;
+
+    private Rigidbody2D rb;
+
+    public bool isGrounded;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(jumpKey) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        isGrounded = Physics2D.OverlapCircle(GroundCheck.position, 0.15f, groundLayer);
+    }
+}
---
+++ ROOOOAAGAAA/Assets/Code/PlayerMovement.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float WalkSpeed;
+    [SerializeField]
+    private KeyCode leftKey;
+    [SerializeField]
+    private KeyCode rightKey;
+
+    private bool isFacingRight = true;
+    private float direction = 0;
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKeyDown(leftKey))
+        {
+            if (isFacingRight)
+            {
+                Flip();
+            }
+
+            direction = -1;
+        } 
+
+        if (Input.GetKeyDown(rightKey))
+        {
+            if (!isFacingRight)
+            {
+                Flip();
+            }
+
+            direction = 1;
+        } 
+
+        if (!Input.GetKey(rightKey) && !Input.GetKey(leftKey))
+        {
+            direction = 0;
+        }
+    }
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        sr.flipX = !isFacingRight;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(WalkSpeed * direction * 10 * Time.deltaTime, rb.velocity.y);
+    }
+}
